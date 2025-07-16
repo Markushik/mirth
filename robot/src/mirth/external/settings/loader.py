f8ee0a915f6ef129207d27d217cff2d644112704
Markushik <zemly@duck.com>
2025-07-16 09:34:32 +0300

    new arch

--- robot/src/mirth/external/settings/loader.py
+++ robot/src/mirth/external/settings/loader.py
@@ -7,8 +7,8 @@ from src.mirth.external.settings import Settings
 def get_settings():
     dynaconf: Dynaconf = Dynaconf(
         settings_files=[
-            "settings/.secrets.toml",
-            "settings/settings.toml",
+            "shared/settings/.secrets.toml",
+            "shared/settings/settings.toml",
         ],
         validators=[
             Validator("bot.token", is_type_of=str),
